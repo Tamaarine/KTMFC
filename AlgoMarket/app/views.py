eb647ca96303b2057731a786da8fe26916949a00
Quinten De Man <quinten.deman@gmail.com>
2022-03-03 17:51:10 -0500

    created reusable customizable navbar partial template and brought in search page

--- AlgoMarket/app/views.py
+++ AlgoMarket/app/views.py
@@ -1,10 +1,27 @@
 from django.shortcuts import render
 
 def index(request):
-    return render(request, 'app/index.html')
+    context = {
+        'nav_bar_name': None,
+        'nav_bar_list': [
+            {'name': 'Login', 'url': '/app/login'},
+            {'name': 'Register', 'url': '/app/register'}
+        ]
+    }
+    return render(request, 'app/index.html', context)
 
 def register(request):
     return render(request, 'app/register.html')
 
 def login(request):
     return render(request, 'app/login.html')
+
+def search(request):
+    context = {
+        'nav_bar_name': None,
+        'nav_bar_list': [
+            {'name': 'Login', 'url': '/app/login'},
+            {'name': 'Register', 'url': '/app/register'}
+        ]
+    }
+    return render(request, 'app/search.html', context)
