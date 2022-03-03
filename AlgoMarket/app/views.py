e9f4d3f12b9d4ebaf0ad8adc9e0d34c20f5b6bff
Quinten De Man <quinten.deman@gmail.com>
2022-03-03 14:56:12 -0500

    added home, register, and login pages to django project and converted urls appropriately

--- AlgoMarket/app/views.py
+++ AlgoMarket/app/views.py
@@ -2,3 +2,9 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'app/index.html')
+
+def register(request):
+    return render(request, 'app/register.html')
+
+def login(request):
+    return render(request, 'app/login.html')
