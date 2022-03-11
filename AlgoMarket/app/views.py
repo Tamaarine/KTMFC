2a0c98a432180f53eb858df2f5ef694841ad1b89
Quinten De Man <quinten.deman@stonybrook.edu>
2022-03-11 18:35:20 -0500

    abstracted list of service cards

--- AlgoMarket/app/views.py
+++ AlgoMarket/app/views.py
@@ -22,6 +22,13 @@ def search(request):
         'nav_bar_list': [
             {'name': 'Login', 'url': '/app/login'},
             {'name': 'Register', 'url': '/app/register'}
+        ],
+        'service_card_list': [
+            {'name': 'Anime Sketches', 'description': 'I draw beautiful anime sketches for Algorand!', 'image_path': 'yes.jpg'},
+            {'name': 'Singing!', 'description': 'I will voice over anything you write for Cryptocurrency', 'image_path': 'sing.jpg'},
+            {'name': 'Tax', 'description': 'I do your taxes for crypto', 'image_path': 'accountant.png'},
+            {'name': 'Graphic DESIGN!', 'description': 'I will make beautiful graphic design for anything', 'image_path': 'design.jpg'},
+            {'name': 'Profession Googler', 'description': 'I am a professional googler and I will google for you', 'image_path': 'google.jpg'}
         ]
     }
     return render(request, 'app/search.html', context)
