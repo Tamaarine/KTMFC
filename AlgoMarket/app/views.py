847fbaafaf95b2bf4d1d0d6548146eb81b3bcea0
dwu1200 <daniel.wu.1@stonybrook.edu>
2022-03-27 23:00:36 -0400

    search functionality (no idea if i did this right but it works)

--- AlgoMarket/app/views.py
+++ AlgoMarket/app/views.py
@@ -30,6 +30,8 @@ def search(request):
             {'name': 'Profession Googler', 'description': 'I am a professional googler and I will google for you', 'image_path': 'google.jpg'}
         ]
     }
+    q = request.GET.get('sch')
+    context['service_list'] = [x for x in context['service_list'] if (q in x['name'] or q in x['description'])]
     return render(request, 'app/search.html', context)
 
 def store(request):
