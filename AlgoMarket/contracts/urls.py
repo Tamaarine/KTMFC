225ca805e13d0776426963de779e862a9cb65099
weiwenzhou <weiwen.zhou@stonybrook.edu>
2022-04-08 18:01:33 -0400

    add base contracts app with helper methods

---
+++ AlgoMarket/contracts/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    # path('', requests.index, name='index')
+    path('create_account/', views.create_account, name="create_account"),
+]
