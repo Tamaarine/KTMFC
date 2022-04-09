f9b1d4c9a655de2962ef9e5c9dfedd7ca3e79077
Tamaarine <irebootplaygt@gmail.com>
2022-04-09 23:52:24 +0000

    Changed the settings.py

--- AlgoMarket/AlgoMarket/settings.py
+++ AlgoMarket/AlgoMarket/settings.py
@@ -44,7 +44,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'app.apps.AppConfig'
+    'app.apps.AppConfig',
+    'contracts.apps.ContractsConfig'
 ]
 
 MIDDLEWARE = [
