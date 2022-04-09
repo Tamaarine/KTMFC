ff353552c11618452ae036d511cdc5aaaea08a99
Tamaarine <irebootplaygt@gmail.com>
2022-04-09 23:53:55 +0000

    Added email for giving to buyer/seller when the purchase is complete

--- AlgoMarket/contracts/views.py
+++ AlgoMarket/contracts/views.py
@@ -5,6 +5,10 @@ from app.models import Service, User
 from contracts.helpers import INITIAL_FUNDS, add_standalone_account, add_transaction, cli_passphrase_for_account, initial_funds_sender
 from contracts.models import Account
 
+from app.utils import *
+import app.utils as utils
+from django.template.loader import render_to_string
+
 # Create your views here.
 def create_account(request, username):
     user = get_object_or_404(User, username=username)
@@ -43,4 +47,33 @@ def purchase(request, sender, store_id):
     error_field, error_description = add_transaction(sender_account.address, store_account.address, sender_account.passphrase, store.price*10000, f"store {store.id}")
     if error_field != "":
         raise Http404(error_description)
+        
+    # Add email to notify the seller about the purchase
+    service = Service.objects.get(pk=store_id)
+    
+    mail_subject = "AlgoMarket - A Customer Has Purchase Your Service!"
+    message = render_to_string('app/to_seller_email_template.html', {
+        'seller': service.seller.username,
+        'buyer': request.user.username,
+        'buyer_email': request.user.email,
+        'service_name': service.name,
+        'service_description': service.description,
+        'service_price': service.price,
+        'store_num': store_id
+    })
+    to_email = service.seller.email
+    utils.email(mail_subject, message, to_email)
+    
+    mail_subject = "AlgoMarket - Service Purchase Confirmation!"
+    message = render_to_string('app/to_buyer_email_template.html', {
+        'username': request.user.username,
+        'seller_email': service.seller.email,
+        'service_name': service.name,
+        'service_description': service.description,
+        'service_price': service.price,
+        'store_num': store_id
+    })
+    to_email = request.user.email
+    utils.email(mail_subject, message, to_email)
+    
     return redirect("accounts", request.user.username)
