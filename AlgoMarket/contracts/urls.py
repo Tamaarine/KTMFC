bbcab0d027e8b4a9eb4a9f71728ca58d14d7fd24
weiwenzhou <weiwen.zhou@stonybrook.edu>
2022-04-09 07:39:44 -0400

    add purchase initial

--- AlgoMarket/contracts/urls.py
+++ AlgoMarket/contracts/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('create_account/<str:username>', views.create_account, name="create_account"),
     path('account/<str:address>', views.account_page, name="account_page"),
     path('accounts/<str:username>', views.accounts, name="accounts"),
+    path('purchase/<str:sender>/<int:store_id>', views.purchase, name="purchase"),
 ]
--- AlgoMarket/contracts/views.py
+++ AlgoMarket/contracts/views.py
@@ -1,6 +1,7 @@
+from django.http import Http404
 from django.shortcuts import get_object_or_404, render, redirect
 
-from app.models import User
+from app.models import Service, User
 from contracts.helpers import INITIAL_FUNDS, add_standalone_account, add_transaction, cli_passphrase_for_account, initial_funds_sender
 from contracts.models import Account
 
@@ -25,3 +26,21 @@ def accounts(request, username):
 def account_page(request, address):
     context = {"account": Account.instance_from_address(address)}
     return render(request, "contracts/account_page.html", context)
+
+def purchase(request, sender, store_id):
+    sender_user = get_object_or_404(User, username=sender)
+    sender_account = Account.accounts_from_user(sender_user)
+    if sender_account:
+        sender_account = sender_account[0]
+    else:
+        raise Http404("No accounts found.")
+    store = get_object_or_404(Service, id=store_id)
+    store_account = Account.accounts_from_user(store.seller)
+    if store_account:
+        store_account = store_account[0]
+    else:
+        raise Http404("Seller does not have account setup.")
+    error_field, error_description = add_transaction(sender_account.address, store_account.address, sender_account.passphrase, store.price*10000, f"store {store.id}")
+    if error_field != "":
+        raise Http404(error_description)
+    return redirect("accounts", request.user.username)
